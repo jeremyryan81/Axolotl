d714ea0f7d6afbde90669ce1c3485df5aa295260
Jeremy Thompson <jeremyrthompson81@gmail.com>
2025-09-02 21:47:51 -0600

    Initial Setup

---
+++ resources/js/componentsTemp/ExampleComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="example-component">
+        Hello from Vue! 🎉
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ExampleComponent"
+};
+</script>
+
+<style scoped>
+.example-component {
+    color: #3490dc;
+    font-weight: bold;
+    padding: 1rem;
+    border: 1px solid #3490dc;
+    border-radius: 0.5rem;
+}
+</style>
